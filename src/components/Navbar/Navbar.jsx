73c71df1f19a057e66bf3eba5d3295d4e0d1beda
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2021-11-30 11:17:24 +0200

    Link to cart is working perfectly

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/KingAJ-2.png'
 import useStyles from './styles';
@@ -17,7 +18,7 @@ const Navbar = ({totalItems}) => {
                     </Typography>
                     <div className={classes.grow} />
                     <div className={classes.button}>
-                        <IconButton aria-label="Show Cart Items" color="inherit">
+                        <IconButton component={Link} to="/cart" aria-label="Show Cart Items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart/>
                             </Badge>
