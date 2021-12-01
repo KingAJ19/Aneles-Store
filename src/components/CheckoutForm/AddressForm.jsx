aa8dae735a0f2ea74a6dcd6fe5001c592cee5ecb
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2021-12-01 07:24:35 +0200

    checkout id shows the shipping countries

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 import FormInput from './CustomTextField';
@@ -6,7 +6,7 @@ import FormInput from './CustomTextField';
 import { commerce } from '../../lib/commerce';
 
 
-const AddressForm = () => {
+const AddressForm = ({ checkoutToken }) => {
     const [shippingCountries, setShippingCountries] = useState([]);
     const [shippingCountry, setShippingCountry] = useState('');
     const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
@@ -16,11 +16,14 @@ const AddressForm = () => {
     
     const fetchShippingCountries = async (checkoutTokenId) => {
         const {countries} = await commerce.services.localeListShippingCountries(checkoutTokenId);
+        console.log(countries);
         setShippingCountries(countries);
     }
 
     
-    
+    useEffect(() => {
+        fetchShippingCountries(checkoutToken.id)
+    }, []);
     
     const methods = useForm();
     return (
