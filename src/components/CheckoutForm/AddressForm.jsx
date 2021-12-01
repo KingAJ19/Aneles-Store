22f0e66c9f1d58c67d19bea98404d697a7271a81
KingAJ <65874068+KingAJ19@users.noreply.github.com>
2021-12-01 09:15:01 +0200

    added some buttons

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import FormInput from './CustomTextField';
 
 import { commerce } from '../../lib/commerce';
@@ -98,6 +99,11 @@ const AddressForm = ({ checkoutToken }) => {
                             </Select>
                         </Grid>
                     </Grid>
+                    <br/>
+                    <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+                        <Button component={Link} to="/cart" variant="outlined">Back To Cart</Button>
+                        <Button type="submit" variant="contained" color="primary">Next</Button>
+                    </div>
                 </form>
             </FormProvider>
         </>
